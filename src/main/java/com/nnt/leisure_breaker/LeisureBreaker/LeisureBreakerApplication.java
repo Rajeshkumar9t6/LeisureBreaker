806e77e68e9c7a264b3380006712e96e6b20b84f
ARUNKUMAR <arunak283933@gmail.com>
2019-03-14 22:47:53 +0530

    initial commit

---
+++ src/main/java/com/nnt/leisure_breaker/LeisureBreaker/LeisureBreakerApplication.java
@@ -0,0 +1,13 @@
+package com.nnt.leisure_breaker.LeisureBreaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeisureBreakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeisureBreakerApplication.class, args);
+	}
+
+}
